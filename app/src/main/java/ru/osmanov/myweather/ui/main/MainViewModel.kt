9ca616433ab1ddd6e3d35ac6075d542f10868fb1
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-17 13:48:50 +0300

    Добавил связку с LiveData для ViewModel

--- app/src/main/java/ru/osmanov/myweather/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/osmanov/myweather/ui/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package ru.osmanov.myweather.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()) :
+    ViewModel() {
+    fun getData(): LiveData<Any> {
+        return liveDataToObserve
+    }
 }
