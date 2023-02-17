cc2c6c184582f500c0ea0f2fd64c38662bf3d1c8
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-17 14:07:20 +0300

    Создал инстанс Observer, который сразу узнает если данные в LiveData изменятся

--- app/src/main/java/ru/osmanov/myweather/ui/main/MainFragment.kt
+++ app/src/main/java/ru/osmanov/myweather/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import ru.osmanov.myweather.R
 
 class MainFragment : Fragment() {
@@ -20,6 +22,13 @@ class MainFragment : Fragment() {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        val observer = Observer<Any> { renderData(it) } //выполняет метод renderData, как только LiveData обновляет данные, которые она хранит
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        // если Данные, которые хранит LiveData, изменятся, Observer сразу об этом узнает и вызовет метод renderData, куда передаст новые данные.
+    }
+
+    private fun renderData(data: Any?) { //В качестве аргумента renderData принимает объект, возвращаемый LiveData
+        Toast.makeText(context, "data", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateView(
