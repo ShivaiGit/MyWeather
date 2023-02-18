e33040ede094314f69073030758020f5e3d2bc4d
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-18 17:11:21 +0300

    Теперь Success будет возвращать данные класса Weather

--- app/src/main/java/ru/osmanov/myweather/AppState.kt
+++ app/src/main/java/ru/osmanov/myweather/AppState.kt
@@ -1,7 +1,7 @@
 package ru.osmanov.myweather
 
 sealed class AppState {
-    data class Success(val weatherData: Any) : AppState()//данные получены
+    data class Success(val weatherData: Weather) : AppState()//данные получены
     data class Error(val error: Throwable) : AppState()//произошла ошибка
     object Loading : AppState() // идет загрузка данных
 }
