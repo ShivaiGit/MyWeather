d0afe5b21df2678119b4351b30fce470b23368a1
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-18 17:19:15 +0300

    Создаем в ViewModel репозиторий и возвращаем данные о погоде

--- app/src/main/java/ru/osmanov/myweather/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/osmanov/myweather/ui/main/MainViewModel.kt
@@ -3,20 +3,26 @@ package ru.osmanov.myweather.ui.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.osmanov.myweather.AppState
+import ru.osmanov.myweather.Repository
+import ru.osmanov.myweather.RepositoryImpl
 import java.lang.Thread.sleep
 
-class MainViewModel(private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData()) :
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) :
     ViewModel() {
 
     fun getLiveData() = liveDataToObserve
-    fun getWeather() = getDataFromLocalSource()
+    fun getWeatherFromLocaleSource() = getDataFromLocalSource()
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
 
     //Имитирует запрос к БД или ещё какому-то источнику данных в приложении
     private fun getDataFromLocalSource() {
         liveDataToObserve.value = AppState.Loading // меняем состояние приложения на "Идет загрузка"
         Thread {
             sleep(1000)
-            liveDataToObserve.postValue(AppState.Success(Any())) //загрузка успешна
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getWeatherFromLocaleStorage()))
         }.start()
     }
 
