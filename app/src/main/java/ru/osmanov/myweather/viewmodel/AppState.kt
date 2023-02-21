9a291deb41b81c3cb1fa410f3f7314972fe55ed5
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-21 15:30:27 +0300

    Изменил тип данных для AppState

--- app/src/main/java/ru/osmanov/myweather/viewmodel/AppState.kt
+++ app/src/main/java/ru/osmanov/myweather/viewmodel/AppState.kt
@@ -3,7 +3,7 @@ package ru.osmanov.myweather.viewmodel
 import ru.osmanov.myweather.repository.Weather
 
 sealed class AppState {
-    data class Success(val weatherData: Weather) : AppState() //данные получены
+    data class Success(val weatherData: List<Weather>) : AppState() //данные получены
     data class Error(val error: Throwable) : AppState() //произошла ошибка
     object Loading : AppState() //идет загрузка данных
 }
